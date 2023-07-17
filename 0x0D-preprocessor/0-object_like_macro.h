380dda9757c64e61141efe20d67e0f73ccbfac7b
Ola072726 <ume505908@gmail.com>
2023-07-17 15:20:41 -0700

    b

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,15 @@
+0-object_like_macro.h
+
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+#define SIZE 1024
+
+#endif
+fndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+#define SIZE 1024
+
+#endif
+
